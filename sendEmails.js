2bc2684a8a22170bc4729e50e90828a88bd14633
Oliveoil13 <olivequinn13@gmail.com>
2024-01-10 14:46:11 -0700

    sends emails

---
+++ sendEmails.js
@@ -0,0 +1,9 @@
+function sendReferaal(){
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        receiver_email : document.getElementById("receiver_email").value,
+    }
+    emailjs.send("service_tcfwpic","template_wy03itk",parms).then(alert("Email Sent!!"))
+}
