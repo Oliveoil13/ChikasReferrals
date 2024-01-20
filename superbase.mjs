3bc3d711d4e98b091287b022fa599af55156b8a4
Olive Quinn <126294559+Oliveoil13@users.noreply.github.com>
2024-01-20 15:25:21 -0700

    nothings honestly

--- superbase.mjs
+++ superbase.mjs
@@ -1,13 +1,10 @@
 
-import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
+import createClient from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://smstkcoswejjjelwwlfr.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtc3RrY29zd2VqamplbHd3bGZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyOTIyODIsImV4cCI6MjAyMDg2ODI4Mn0.9O5C9Ornt8f21ZzMzFUQgb8j_85RXsJHh-azVQ5CdOI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 const tableName = 'Partners';
-
-
-
 
 
 async function fetchData() {
