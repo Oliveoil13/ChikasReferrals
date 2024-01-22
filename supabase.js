dcf4a1008e7ae89e6f6fa6a0b584d8eeda4ea590
Olive Quinn <126294559+Oliveoil13@users.noreply.github.com>
2024-01-22 12:24:48 -0700

    changed to

---
+++ supabase.js
@@ -0,0 +1,4 @@
+
+//listen to if the form is submitted and then grab the email submitted and then send it to python 
+
+document.getElementById('data').innerHTML = "hey there";
--- supabase.py
+++ supabase.py
@@ -8,3 +8,6 @@ from supabase import create_client, Client
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+
+response = supabase.table('countries').select("*").execute()
